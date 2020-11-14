911f24a4326e582ec9d7c9b244b2a472fa71c2ba
phil10xs <philipigbobagmail.com>
2020-11-14 22:10:12 +0100

    This is the beginning of the Clima app project. I added the Geolocator flutter package and initialise the getter to detect the location of the device

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:clima/screens/loading_screen.dart';
 
 void main() => runApp(MyApp());
 
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
-
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    getLocation();
   }
-}
+
+  void getLocation() async {
+    try{
+  Position position = await Geolocator().getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+  catch (e){
+      print(e);
+  }
+  }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+      );
+    }
+  }
